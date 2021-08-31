1126e15d384ac93865a5ba7b29bd1e901a522a4e
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-08-31 13:52:25 +0530

    Create LongestPalindromicString.java

---
+++ Leetcode/LongestPalindromicString.java
@@ -0,0 +1,36 @@
+public class Main
+{
+	
+    public String longestPalindrome(String s) {
+        
+        if(s==null || s.length()<1)
+        {
+            return "";
+        }
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int len1 = pal_len(s,i,i);
+            int len2 = pal_len(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if(len > end-start)
+            {
+                start = i - ((len-1)/2);
+                end = i + (len/2);
+            }
+        }
+        return s.substring(start,end+1);
+    }
+    public int pal_len(String s, int left, int right)
+    {
+        if(s==null || left > right)
+            return 0;
+        while(left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right))
+        {
+            left--;
+            right++;
+        }
+        return right - left -1;
+    }
+}
