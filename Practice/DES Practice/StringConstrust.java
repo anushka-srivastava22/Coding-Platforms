3c07d1984d5d81530e41b1bb097c2e4435ab46d6
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-09-07 13:34:30 +0530

    Create StringConstrust.java

---
+++ Practice/DES Practice/StringConstrust.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++){
+            String s = in.next();
+            System.out.println(s.chars().distinct().count());
+        }
+    }
+}
