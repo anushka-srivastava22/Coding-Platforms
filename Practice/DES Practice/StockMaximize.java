0123dea9205ed3ddfaae7c2dc59a8e901a7ad06a
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-09-07 13:28:23 +0530

    Create StockMaximize.java

---
+++ Practice/DES Practice/StockMaximize.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            int numOfDays = scanner.nextInt();
+            int[] prices = new int[numOfDays];
+            for (int j = 0; j < numOfDays; j++) {
+                prices[j] = scanner.nextInt();
+            }
+            System.out.println(getMaxProfit(prices));
+        }
+    }
+
+    public static long getMaxProfit(int[] prices) {
+        long profit = 0L;
+        int maxSoFar = 0;
+        for (int i = prices.length - 1; i > -1 ; i--) {
+            if (prices[i] >= maxSoFar) {
+                maxSoFar = prices[i];
+            }
+            profit += maxSoFar - prices[i];
+        }
+        return profit;
+    }
+    
+}
