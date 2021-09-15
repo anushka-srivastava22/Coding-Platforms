b86c2be7d071f4aad0cb9bd42de634b2f71d5a04
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-09-15 15:31:06 +0530

    Create ShuffleParties.java

---
+++ CodeChef/ShuffleParties.java
@@ -0,0 +1,35 @@
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T--!=0)
+		{
+		 int i,x,y;
+		int n = sc.nextInt();
+		int[] a=new int[n];
+		for(i=0;i<n;i++)
+		{
+		    a[i]=sc.nextInt();
+		}
+		int even = n/2;
+		int odd = n-(n/2);
+		
+		int o = 0;
+		int c = 0;
+		for(i=0;i<n;i++)
+		{
+		if(a[i]%2==1)
+		    o++;
+		else
+	    	c++;
+		}
+		if(o<even)
+		 x=o;
+		else 
+		 x=even;
+		if(c<odd)
+		 y=c;
+		else 
+		 y=odd;
+		System.out.println(x+y);
+		    
+		}
