c268c25c719b6779517ccdd78264ac61a256dd28
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-09-17 13:49:36 +0530

    Create ContainDuplicate.java

---
+++ Leetcode/Array/ContainDuplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null || nums.length == 0)
+        {
+            return false;
+        }
+        HashSet<Integer> set=new HashSet<Integer>();
+        for(int i:nums)
+        {
+            if(!set.add(i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
