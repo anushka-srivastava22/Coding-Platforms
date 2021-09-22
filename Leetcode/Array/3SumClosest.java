371ad28e1b384344d167d1247f71a59d587b7500
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-09-22 15:36:17 +0530

    Create 3SumClosest.java

---
+++ Leetcode/Array/3SumClosest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+         int diff = Integer.MAX_VALUE, sz = nums.length;
+         Arrays.sort(nums);
+        for (int i = 0; i < sz && diff != 0; ++i) {
+            int lo = i + 1, hi = sz - 1;
+            while (lo < hi) {
+                int sum = nums[i] + nums[lo] + nums[hi];
+                if (Math.abs(target - sum) < Math.abs(diff))
+                    diff = target - sum;
+                if (sum < target)
+                    ++lo;
+                else
+                    --hi;
+            }
+        }
+        return target - diff;
+    }
+}
