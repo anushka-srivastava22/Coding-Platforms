5b8cf83ef7c5ee9aa65b9b78fccf0f4d75a9be39
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-10-14 19:41:57 +0530

    Create PerfectSquares.java

---
+++ Leetcode/DynamicProgramming/PerfectSquares.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int numSquares(int n) {
+        if (n <= 3)
+            return n;
+ 
+        // Create a dynamic programming
+        // table
+        // to store sq
+        int dp[] = new int[n + 1];
+ 
+        // getMinSquares table for
+        // base case entries
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+ 
+        // getMinSquares rest of the
+        // table using recursive
+        // formula
+        for (int i = 4; i <= n; i++)
+        {
+         
+            // max value is i as i can
+            // always be represented
+            // as 1*1 + 1*1 + ...
+            dp[i] = i;
+ 
+            // Go through all smaller numbers to
+            // to recursively find minimum
+            for (int x = 1; x <= Math.ceil(Math.sqrt(i)); x++)
+            {
+                int temp = x * x;
+                if (temp > i)
+                    break;
+                else
+                    dp[i] = Math.min(dp[i], 1 + dp[i - temp]);
+            }
+        }
+ 
+        // Store result and free dp[]
+        int res = dp[n];
+ 
+        return res;
+    }
+}
