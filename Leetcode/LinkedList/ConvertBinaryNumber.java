a0b0373f9771f68e83c611f2a4657c6d97ac3c23
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-11-23 11:25:37 +0530

    Create ConvertBinaryNumber.java

---
+++ Leetcode/LinkedList/ConvertBinaryNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int length = getLength(head);
+    int sum = 0;
+    while (head != null) {
+      sum += ((int) Math.pow(2, length - 1)) * head.val;
+      length--;
+      head = head.next;
+    }
+    return sum;
+    }
+    private int getLength(ListNode head) {
+    int count = 0;
+    while (head != null) {
+      count++;
+      head = head.next;
+    }
+    return count;
+  }
+}
