61efc8aad6ce4cde6934ad324df31c7b4d227c65
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-11-25 19:29:35 +0530

    Create RemoveNthNodeFromLast.java

---
+++ Leetcode/LinkedList/RemoveNthNodeFromLast.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null)
+        return null;
+ 
+    ListNode fast = head;
+    ListNode slow = head;
+ 
+    for(int i=0; i<n; i++){
+        fast = fast.next;
+    }
+ 
+    //if remove the first node
+    if(fast == null){
+        head = head.next;
+        return head;
+    }
+ 
+    while(fast.next != null){
+        fast = fast.next;
+        slow = slow.next;
+    }
+ 
+    slow.next = slow.next.next;
+ 
+    return head;
+    }
+}
