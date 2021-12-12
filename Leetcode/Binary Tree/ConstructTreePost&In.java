9183fd6d45746dfb3fd87ec56a47e114363e4fd0
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2021-12-12 13:10:23 +0530

    Create ConstructTreePost&In.java

---
+++ Leetcode/Binary Tree/ConstructTreePost&In.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int index;
+
+    public static TreeNode buildTree(int[] inorder, int[] postorder, int start, int end, Map<Integer, Integer> map) {
+        if (start > end)
+            return null;
+        int curr = postorder[index--];
+        TreeNode node = new TreeNode(curr);
+        if (start == end)
+            return node;
+
+        int inIndex = map.get(curr);
+        TreeNode right = buildTree(inorder, postorder, inIndex + 1, end, map);
+        TreeNode left = buildTree(inorder, postorder, start, inIndex - 1, map);
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        index = 0;
+        for (int i = 0; i < inorder.length; i++)
+            map.put(inorder[i], i);
+        index = postorder.length - 1;
+        return buildTree(inorder, postorder, 0, postorder.length - 1, map);
+    }
+
+    
+}   
